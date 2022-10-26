84caab3b0b3e1453967fd6af3a5f1dfc765d7de3
sombrerosheep <sombrerosheep@gmail.com>
2022-10-25 22:31:02 -0500

    add parameter names where not obvious

--- storage.go
+++ storage.go
@@ -5,28 +5,28 @@ type Storage interface {
 	Close() error
 
 	// Links
-	GetLinksForPart(int64) ([]Link, error)
-	GetLinksForKit(int64) ([]Link, error)
-	AddLinkToPart(int64, string) (Link, error)
-	AddLinkToKit(int64, string) (Link, error)
-	RemoveLinkFromPart(int64, int64) error
-	RemoveLinkFromKit(int64, int64) error
+	GetLinksForPart(partId int64) ([]Link, error)
+	GetLinksForKit(kitId int64) ([]Link, error)
+	AddLinkToPart(partId int64, url string) (Link, error)
+	AddLinkToKit(kitId int64, url string) (Link, error)
+	RemoveLinkFromPart(partId int64, linkId int64) error
+	RemoveLinkFromKit(kitId int64, linkId int64) error
 
 	// Parts
 	GetParts() ([]Part, error)
-	GetPart(int64) (Part, error)
+	GetPart(partId int64) (Part, error)
 	AddPart(Part) (Part, error)
 	UpdatePart(Part) (Part, error)
-	DeletePart(int64) error
+	DeletePart(partId int64) error
 
 	// Kits
 	GetKits() ([]Kit, error)
-	GetKit(int64) (Kit, error)
+	GetKit(kitId int64) (Kit, error)
 	AddKit(Kit) (Kit, error)
 	UpdateKit(Kit) (Kit, error)
-	DeleteKit(int64) error
-	GetKitParts(int64) ([]KitPart, error)
-	AddPartToKit(int64, int64, uint64) error
-	SetPartQuantityForKit(int64, uint64, int64) error
-	RemovePartFromKit(int64, int64) error
+	DeleteKit(kitId int64) error
+	GetKitParts(kitId int64) ([]KitPart, error)
+	AddPartToKit(partId int64, kitId int64, quantity uint64) error
+	SetPartQuantityForKit(partId int64, kitId uint64, quantity int64) error
+	RemovePartFromKit(partId int64, kitId int64) error
 }
