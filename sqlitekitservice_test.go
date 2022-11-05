b8e2dff4288277b109baf3ecc8f7370c4a8fe11a
sombrerosheep <sombrerosheep@gmail.com>
2022-11-04 21:37:20 -0500

    subtest consistency

--- sqlitekitservice_test.go
+++ sqlitekitservice_test.go
@@ -33,7 +33,7 @@ var exampleKits = [...]Kit{
 
 func (db greenSqliteMock) GetKit(kitId int64) (Kit, error) {
 	if kitId <= int64(len(exampleKits)) && kitId > 0 {
-		return exampleKits[kitId - 1], nil
+		return exampleKits[kitId-1], nil
 	}
 	return exampleKits[0], nil
 }
@@ -85,162 +85,180 @@ func (db greenSqliteMock) RemoveKit(kitId int64) error {
 }
 
 func Test_sqlitekitservice_GetAll(t *testing.T) {
-	sut := SqliteKitService{
-		db: greenSqliteMock{},
-		partservice: SqlitePartService{
+	t.Run("When no errors are returned", func(t *testing.T) {
+		sut := SqliteKitService{
 			db: greenSqliteMock{},
-		},
-	}
+			partservice: SqlitePartService{
+				db: greenSqliteMock{},
+			},
+		}
 
-	expectedKitParts := make([]KitPart, len(exampleKitParts))
-	for i := range exampleKitParts {
-		kp := exampleKitParts[i]
-		kp.Part.Links = exampleLinks[:]
+		expectedKitParts := make([]KitPart, len(exampleKitParts))
+		for i := range exampleKitParts {
+			kp := exampleKitParts[i]
+			kp.Part.Links = exampleLinks[:]
 
-		expectedKitParts[i] = kp
-	}
+			expectedKitParts[i] = kp
+		}
 
-	expectedKits := make([]Kit, len(exampleKits))
-	for i := range exampleKits {
-		k := exampleKits[i]
-		k.Parts = expectedKitParts[:]
-		k.Links = exampleLinks[:]
+		expectedKits := make([]Kit, len(exampleKits))
+		for i := range exampleKits {
+			k := exampleKits[i]
+			k.Parts = expectedKitParts[:]
+			k.Links = exampleLinks[:]
 
-		expectedKits[i] = k
-	}
+			expectedKits[i] = k
+		}
 
-	kits, err := sut.GetAll()
+		kits, err := sut.GetAll()
 
-	assert.Nil(t, err)
-	assert.Equal(t, expectedKits, kits)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedKits, kits)
+	})
 }
 
 func Test_sqlitekitservice_Get(t *testing.T) {
-	sut := SqliteKitService{
-		db: greenSqliteMock{},
-		partservice: SqlitePartService{
+	t.Run("When no errors are returned", func(t *testing.T) {
+		sut := SqliteKitService{
 			db: greenSqliteMock{},
-		},
-	}
+			partservice: SqlitePartService{
+				db: greenSqliteMock{},
+			},
+		}
 
-	expectedKitParts := make([]KitPart, len(exampleKitParts))
-	for i := range exampleKitParts {
-		kp := exampleKitParts[i]
-		kp.Part.Links = exampleLinks[:]
+		expectedKitParts := make([]KitPart, len(exampleKitParts))
+		for i := range exampleKitParts {
+			kp := exampleKitParts[i]
+			kp.Part.Links = exampleLinks[:]
 
-		expectedKitParts[i] = kp
-	}
+			expectedKitParts[i] = kp
+		}
 
-	expectedKit := exampleKits[0]
-	expectedKit.Parts = expectedKitParts[:]
-	expectedKit.Links = exampleLinks[:]
+		expectedKit := exampleKits[0]
+		expectedKit.Parts = expectedKitParts[:]
+		expectedKit.Links = exampleLinks[:]
 
-	kit, err := sut.Get(expectedKit.ID)
-	
-	assert.Nil(t, err)
-	assert.Equal(t, expectedKit, kit)
+		kit, err := sut.Get(expectedKit.ID)
+
+		assert.Nil(t, err)
+		assert.Equal(t, expectedKit, kit)
+	})
 }
 
 func Test_sqlitekitservice_AddLink(t *testing.T) {
-	sut := SqliteKitService{
-		db: greenSqliteMock{},
-		partservice: SqlitePartService{
+	t.Run("When no errors are returned", func(t *testing.T) {
+		sut := SqliteKitService{
 			db: greenSqliteMock{},
-		},
-	}
+			partservice: SqlitePartService{
+				db: greenSqliteMock{},
+			},
+		}
 
-	expectedLink := exampleLinks[0]
+		expectedLink := exampleLinks[0]
 
-	link, err := sut.AddLink(exampleKits[0].ID, exampleLinks[0].URL)
+		link, err := sut.AddLink(exampleKits[0].ID, exampleLinks[0].URL)
 
-	assert.Nil(t, err)
-	assert.Equal(t, expectedLink, link)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedLink, link)
+	})
 }
 
 func Test_sqlitekitservice_RemoveLink(t *testing.T) {
-	sut := SqliteKitService{
-		db: greenSqliteMock{},
-		partservice: SqlitePartService{
+	t.Run("When no errors are returned", func(t *testing.T) {
+		sut := SqliteKitService{
 			db: greenSqliteMock{},
-		},
-	}
+			partservice: SqlitePartService{
+				db: greenSqliteMock{},
+			},
+		}
 
-	err := sut.RemoveLink(exampleKits[0].ID, exampleLinks[0].ID)
+		err := sut.RemoveLink(exampleKits[0].ID, exampleLinks[0].ID)
 
-	assert.Nil(t, err)
+		assert.Nil(t, err)
+	})
 }
 
 func Test_sqlitekitservice_AddPart(t *testing.T) {
-	sut := SqliteKitService{
-		db: greenSqliteMock{},
-		partservice: SqlitePartService{
+	t.Run("When no errors are returned", func(t *testing.T) {
+		sut := SqliteKitService{
 			db: greenSqliteMock{},
-		},
-	}
+			partservice: SqlitePartService{
+				db: greenSqliteMock{},
+			},
+		}
 
-	err := sut.RemovePart(exampleKits[0].ID, exampleParts[0].ID)
+		err := sut.RemovePart(exampleKits[0].ID, exampleParts[0].ID)
 
-	assert.Nil(t, err)
+		assert.Nil(t, err)
+	})
 }
 
 func Test_sqlitekitservice_SetPartQuantity(t *testing.T) {
-	sut := SqliteKitService{
-		db: greenSqliteMock{},
-		partservice: SqlitePartService{
+	t.Run("When no errors are returned", func(t *testing.T) {
+		sut := SqliteKitService{
 			db: greenSqliteMock{},
-		},
-	}
+			partservice: SqlitePartService{
+				db: greenSqliteMock{},
+			},
+		}
 
-	err := sut.SetPartQuantity(exampleKits[0].ID, exampleParts[0].ID, 42)
+		err := sut.SetPartQuantity(exampleKits[0].ID, exampleParts[0].ID, 42)
 
-	assert.Nil(t, err)
+		assert.Nil(t, err)
+	})
 }
 
 func Test_sqlitekitservice_RemovePart(t *testing.T) {
-	sut := SqliteKitService{
-		db: greenSqliteMock{},
-		partservice: SqlitePartService{
+	t.Run("When no errors are returned", func(t *testing.T) {
+		sut := SqliteKitService{
 			db: greenSqliteMock{},
-		},
-	}
+			partservice: SqlitePartService{
+				db: greenSqliteMock{},
+			},
+		}
 
-	err := sut.RemovePart(exampleKits[0].ID, exampleParts[0].ID)
+		err := sut.RemovePart(exampleKits[0].ID, exampleParts[0].ID)
 
-	assert.Nil(t, err)
+		assert.Nil(t, err)
+	})
 }
 
 func Test_sqlitekitservice_New(t *testing.T) {
-	sut := SqliteKitService{
-		db: greenSqliteMock{},
-		partservice: SqlitePartService{
+	t.Run("When no errors are returned", func(t *testing.T) {
+		sut := SqliteKitService{
 			db: greenSqliteMock{},
-		},
-	}
+			partservice: SqlitePartService{
+				db: greenSqliteMock{},
+			},
+		}
 
-	expectedKit := Kit{
-		ID:        exampleKits[0].ID,
-		Parts:     []KitPart{},
-		Name:      exampleKits[0].Name,
-		Schematic: exampleKits[0].Schematic,
-		Diagram:   exampleKits[0].Diagram,
-		Links:     []Link{},
-	}
+		expectedKit := Kit{
+			ID:        exampleKits[0].ID,
+			Parts:     []KitPart{},
+			Name:      exampleKits[0].Name,
+			Schematic: exampleKits[0].Schematic,
+			Diagram:   exampleKits[0].Diagram,
+			Links:     []Link{},
+		}
 
-	kit, err := sut.New(expectedKit.Name, expectedKit.Schematic, expectedKit.Diagram)
+		kit, err := sut.New(expectedKit.Name, expectedKit.Schematic, expectedKit.Diagram)
 
-	assert.Nil(t, err)
-	assert.Equal(t, expectedKit, kit)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedKit, kit)
+	})
 }
 
 func Test_sqlitekitservice_Delete(t *testing.T) {
-	sut := SqliteKitService{
-		db: greenSqliteMock{},
-		partservice: SqlitePartService{
+	t.Run("When no errors are returned", func(t *testing.T) {
+		sut := SqliteKitService{
 			db: greenSqliteMock{},
-		},
-	}
+			partservice: SqlitePartService{
+				db: greenSqliteMock{},
+			},
+		}
 
-	err := sut.Delete(exampleKits[0].ID)
+		err := sut.Delete(exampleKits[0].ID)
 
-	assert.Nil(t, err)
+		assert.Nil(t, err)
+	})
 }
